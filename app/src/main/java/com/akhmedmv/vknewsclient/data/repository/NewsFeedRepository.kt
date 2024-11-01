2c5b69734c0beee2662a36523388df33450e92e0
Akhmedmv <ahmazelem@gmail.com>
2024-11-01 17:54:02 +0300

    replaced MutableLiveData with Coroutine Flow

--- app/src/main/java/com/akhmedmv/vknewsclient/data/repository/NewsFeedRepository.kt
+++ app/src/main/java/com/akhmedmv/vknewsclient/data/repository/NewsFeedRepository.kt
@@ -8,42 +8,70 @@ import com.akhmedmv.vknewsclient.domain.FeedPost
 import com.akhmedmv.vknewsclient.domain.PostComment
 import com.akhmedmv.vknewsclient.domain.StatisticItem
 import com.akhmedmv.vknewsclient.domain.StatisticType
+import com.akhmedmv.vknewsclient.extentions.mergeWith
 import com.vk.api.sdk.VKPreferencesKeyValueStorage
 import com.vk.api.sdk.auth.VKAccessToken
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
 
 class NewsFeedRepository(application: Application) {
 
     private val storage = VKPreferencesKeyValueStorage(application)
     private val token = VKAccessToken.restore(storage)
+    private val coroutineScope = CoroutineScope(Dispatchers.Default)
+    private val nextDataNeededEvents = MutableSharedFlow<Unit>(replay = 1)
+    private val refreshedListFlow = MutableSharedFlow<List<FeedPost>>()
+    private val loadedListFlow = flow {
+        nextDataNeededEvents.emit(Unit)
+        nextDataNeededEvents.collect {
+            val startFrom = nextFrom
+            if (startFrom == null && feedPosts.isNotEmpty()) {
+                emit(feedPosts)
+                return@collect
+            }
+            val response = if (startFrom == null) {
+                apiService.loadRecommendations(getAccessToken())
+            } else {
+                apiService.loadRecommendations(getAccessToken(), startFrom)
+            }
+            nextFrom = response.newsFeedContent.nextFrom
+            val posts = mapper.mapResponseToPosts(response)
+            if (posts.isNotEmpty()) {
+                _feedPosts.clear()
+                _feedPosts.addAll(posts)
+                posts
+            } else {
+                Log.w("NewsFeedRepository", "Получен пустой список постов")
+                emptyList()
+            }
+            emit(feedPosts)
+        }
+    }
 
     private val apiService = ApiFactory.apiService
     private val mapper = NewsFeedMapper()
 
     private val _feedPosts = mutableListOf<FeedPost>()
-    val feedPosts: List<FeedPost>
+    private val feedPosts: List<FeedPost>
         get() = _feedPosts.toList()
 
     private var nextFrom: String? = null
 
-    suspend fun loadRecommendations(): List<FeedPost> {
-        val startFrom = nextFrom
-        if (startFrom == null && feedPosts.isNotEmpty()) return feedPosts
-        val response = if (startFrom == null) {
-            apiService.loadRecommendations(getAccessToken())
-        } else {
-            apiService.loadRecommendations(getAccessToken(), startFrom)
-        }
-        nextFrom = response.newsFeedContent.nextFrom
-        val posts = mapper.mapResponseToPosts(response)
-        if (posts.isNotEmpty()) {
-            _feedPosts.clear()
-            _feedPosts.addAll(posts)
-            posts
-        } else {
-            Log.w("NewsFeedRepository", "Получен пустой список постов")
-            emptyList()
-        }
-        return feedPosts
+    val recommendations: StateFlow<List<FeedPost>> = loadedListFlow
+        .mergeWith(refreshedListFlow)
+        .stateIn(
+            scope = coroutineScope,
+            started = SharingStarted.Lazily,
+            initialValue = feedPosts
+        )
+
+    suspend fun loadNextData() {
+        nextDataNeededEvents.emit(Unit)
     }
 
     private fun getAccessToken(): String {
@@ -60,6 +88,7 @@ class NewsFeedRepository(application: Application) {
             postId = feedPost.id
         )
         _feedPosts.remove(feedPost)
+        refreshedListFlow.emit(feedPosts)
     }
 
     suspend fun getComments(feedPost: FeedPost): List<PostComment> {
@@ -93,5 +122,6 @@ class NewsFeedRepository(application: Application) {
         val newPost = feedPost.copy(statistics = newStatistics, isLiked = !feedPost.isLiked)
         val postIndex = _feedPosts.indexOf(feedPost)
         _feedPosts[postIndex] = newPost
+        refreshedListFlow.emit(feedPosts)
     }
 }
--- app/src/main/java/com/akhmedmv/vknewsclient/presentation/news/NewsFeedScreen.kt
+++ app/src/main/java/com/akhmedmv/vknewsclient/presentation/news/NewsFeedScreen.kt
@@ -24,7 +24,7 @@ import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -40,7 +40,7 @@ fun NewsFeedScreen(
     onCommentClickListener: (FeedPost) -> Unit
 ) {
     val viewModel: NewsFeedViewModel = viewModel()
-    val screenState = viewModel.screenState.observeAsState(NewsFeedScreenState.Initial)
+    val screenState = viewModel.screenState.collectAsState(NewsFeedScreenState.Initial)
 
     when (val currentState = screenState.value) {
         is NewsFeedScreenState.Posts -> {
